9bfa9702e47e7609a70707adfbcc3322b97efdf6
Gokul Dev P <75772147+gokuldevp@users.noreply.github.com>
2021-10-20 06:44:51 +0530

    jest completed node module.

---
+++ Section 18 Node.js/Node-intro/superfight/index.js
@@ -0,0 +1,7 @@
+const superheroes = require("superheroes");  // get superheroes name
+const supervillains = require("supervillains");  // get supervillains name
+
+mySuperhero = superheroes.random();  //get random superhero name 
+mySupervillain = supervillains.random();  //ger random supervillain name
+
+console.log("Today the fight is between the Hero '"+mySuperhero+"' vs The Villain '"+mySupervillain+"'!")  //show the name of the superhero and the name of supervillain fighting.
